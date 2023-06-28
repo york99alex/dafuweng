a6e976e69be94b863626895def2b6aae77476f63
york99alex <814736838@qq.com>
2023-06-29 00:25:47 +0800

    fix bug

--- scripts/vscripts/utils.lua
+++ scripts/vscripts/utils.lua
@@ -107,6 +107,20 @@ function shallowcopy(orig)
 	return copy
 end
 
+-- table max
+function Table_maxn(t)
+    local mn=nil;
+    for k, v in pairs(t) do
+      if(mn==nil) then
+        mn=v
+      end
+      if mn < v then
+        mn = v
+      end
+    end
+    return mn
+end
+
 -- table覆盖
 function TableOverride(mainTable, table)
 	if mainTable == nil then
